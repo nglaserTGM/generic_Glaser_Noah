66b533cf845f3e58ddcbfc469a7ae1ef11d0279c
Noah Glaser <nglaser@student.tgm.ac.at>
2025-02-26 07:51:55 +0100

    Constructor ertellt mit defoult wert

--- src/src/Stack.java
+++ src/src/Stack.java
@@ -1,2 +1,18 @@
-public class Stack {
+public class Stack<T> {
+    private Object[] elements; // Generic array workaround
+    private int top;
+    private int capacity;
+
+    public Stack(int capacity) {
+        this.capacity = capacity;
+        this.elements = new Object[capacity];
+        this.top = -1;
+    }
+
+    public Stack() {
+        this(10);
+    }
+
+
+
 }
