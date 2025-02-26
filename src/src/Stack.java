37de496e8837eab8513b5d2ba36d0e21bb9081f1
Noah Glaser <nglaser@student.tgm.ac.at>
2025-02-26 08:10:54 +0100

    Stack Klasse vervollstängigt

--- src/src/Stack.java
+++ src/src/Stack.java
@@ -12,7 +12,39 @@ public class Stack<T> {
     public Stack() {
         this(10);
     }
+    public void push(T item) throws StackFullException {
+        if (top == capacity - 1) {
+            throw new StackFullException("Stack is full");
+        }
+        elements[++top] = item;
+    }
 
+
+    @SuppressWarnings("unchecked")
+    public T pop() throws StackEmptyException {
+        if (top == -1) {
+            throw new StackEmptyException("Stack is empty");
+        }
+        return (T) elements[top--];
+    }
+
+
+    @SuppressWarnings("unchecked")
+    public T peek() throws StackEmptyException {
+        if (top == -1) {
+            throw new StackEmptyException("Stack is empty");
+        }
+        return (T) elements[top];
+    }
+
+
+    public String list() {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i <= top; i++) {
+            result.append(elements[i]).append(";");
+        }
+        return result.length() > 0 ? result.substring(0, result.length() - 1) : "";
+    }
 
 
 }
