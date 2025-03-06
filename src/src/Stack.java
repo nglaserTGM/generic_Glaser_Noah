856c5d82bf093467f2893a1b65ad4d491032002b
Noah Glaser <nglaser@student.tgm.ac.at>
2025-03-06 09:50:54 +0100

    Kommentare hinzugefügt

--- src/src/Stack.java
+++ src/src/Stack.java
@@ -1,50 +1,60 @@
 public class Stack<T> {
-    private Object[] elements; // Generic array workaround
+    // An array to store stack elements (using Object as a generic workaround)
+    private Object[] elements;
+    // An integer to track the top position in the stack
     private int top;
+    // Maximum capacity of the stack
     private int capacity;
 
+    // Constructor that sets stack capacity
     public Stack(int capacity) {
         this.capacity = capacity;
-        this.elements = new Object[capacity];
-        this.top = -1;
+        this.elements = new Object[capacity]; // Creates an array with the given capacity
+        this.top = -1; // Stack is empty at the start
     }
 
+    // Default constructor with a capacity of 10
     public Stack() {
         this(10);
     }
+
+    // Method to add an item to the stack
     public void push(T item) throws StackFullException {
+        // Check if the stack is full
         if (top == capacity - 1) {
-            throw new StackFullException("Stack is full");
+            throw new StackFullException("Stack is full"); // Throw an exception if full
         }
-        elements[++top] = item;
+        elements[++top] = item; // Add item and move the top up
     }
 
-
+    // Method to remove and return the top item from the stack
     @SuppressWarnings("unchecked")
     public T pop() throws StackEmptyException {
+        // Check if the stack is empty
         if (top == -1) {
-            throw new StackEmptyException("Stack is empty");
+            throw new StackEmptyException("Stack is empty"); // Throw an exception if empty
         }
-        return (T) elements[top--];
+        return (T) elements[top--]; // Return the top item and move the top down
     }
 
-
+    // Method to see the top item without removing it
     @SuppressWarnings("unchecked")
     public T peek() throws StackEmptyException {
+        // Check if the stack is empty
         if (top == -1) {
-            throw new StackEmptyException("Stack is empty");
+            throw new StackEmptyException("Stack is empty"); // Throw an exception if empty
         }
-        return (T) elements[top];
+        return (T) elements[top]; // Return the top item without removing it
     }
 
-
+    // Method to list all items in the stack as a string
     public String list() {
         StringBuilder result = new StringBuilder();
+        // Loop through all items in the stack
         for (int i = 0; i <= top; i++) {
-            result.append(elements[i]).append(";");
+            result.append(elements[i]).append(";"); // Add items to the string
         }
+        // Remove the last ";" if there are items, or return an empty string
         return result.length() > 0 ? result.substring(0, result.length() - 1) : "";
     }
-
-
 }
--- src/src/StackTest.java
+++ src/src/StackTest.java
@@ -1,7 +1,7 @@
 public class StackTest {
     public static void main(String[] args) {
         try {
-
+            // Test with Integer stack
             System.out.println("Testing Integer Stack:");
             Stack<Integer> numberStack = new Stack<>(3);
             numberStack.push(1);
@@ -13,7 +13,7 @@ public class StackTest {
             System.out.println("Peeked: " + numberStack.peek());
             System.out.println("Stack content: " + numberStack.list());
 
-
+            // Test with String stack
             System.out.println("\nTesting String Stack:");
             Stack<String> textStack = new Stack<>(2);
             textStack.push("Hello");
@@ -24,12 +24,13 @@ public class StackTest {
             System.out.println("Peeked: " + textStack.peek());
             System.out.println("Stack content: " + textStack.list());
 
-
+            // Test StackEmptyException
             System.out.println("\nTesting StackEmptyException:");
-            textStack.pop();
-            textStack.pop();
+            textStack.pop(); // This should work
+            textStack.pop(); // This should cause an exception
 
         } catch (StackFullException | StackEmptyException e) {
+            // Print error message if an exception is caught
             System.err.println("Error: " + e.getMessage());
         }
     }
